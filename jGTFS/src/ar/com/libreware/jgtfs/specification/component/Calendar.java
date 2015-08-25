e800872590e1b0cecd1849e2f60d1fea6e431934
Gonzalo G. Costa <gonzalogcosta@yahoo.com.ar>
2015-08-24 21:00:18 -0300

    Implementación inicial de Calendar y CalendarDate

--- jGTFS/src/ar/com/libreware/jgtfs/specification/component/Calendar.java
+++ jGTFS/src/ar/com/libreware/jgtfs/specification/component/Calendar.java
@@ -1,5 +1,105 @@
 package ar.com.libreware.jgtfs.specification.component;
 
-public class Calendar {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Calendar extends AbstractComponent{
+
+	public enum Days{
+		Monday,Tuesday,Wednesday,Thursday,Friday,Saturday,Sunday
+	}
+	
+	private String id;
+	private Set<Days> activeDays;
+	private Date start,end;
+	
+	public Calendar(){
+		
+	}
+	
+	/**
+	 * Crea un objeto Calendar, que representa los dias que esta disponible/activo un serivicio en un rango de fechas establecido.
+	 * @param id
+	 * @param start fecha que indica el comienzo de la vigencia del servicio
+	 * @param end fecha que indica el fin de la vigencia del servicio
+	 * @param activeDays listado de dias en los cuales el servicio esta disponible
+	 */
+	public Calendar(String id,Date start,Date end, Days... activeDays){
+		this.id = id;
+		this.start = start;
+		this.end = end;
+		
+		this.activeDays = new TreeSet<Days>();
+		this.addDays(activeDays);
+	}
+	
+	@Override
+	public boolean validate() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void copy(AbstractComponent otherComponent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int compareTo(AbstractComponent otherComponent) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean equals(AbstractComponent otherComponent) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public Set<Days> getDays() {
+		return activeDays;
+	}
+
+	public void setDays(Set<Days> activeDays) {
+		this.activeDays = activeDays;
+	}
+
+	public Date getStart() {
+		return start;
+	}
+
+	public void setStart(Date start) {
+		this.start = start;
+	}
+
+	public Date getEnd() {
+		return end;
+	}
+
+	public void setEnd(Date end) {
+		this.end = end;
+	}
+	
+	public void addDays(Days... activeDays){
+		this.activeDays.addAll(Arrays.asList(activeDays));
+	}
+	
+	public void removeDay(Days activeDay){
+		this.activeDays.remove(activeDay);
+	}
 
 }
--- jGTFS/src/ar/com/libreware/jgtfs/specification/component/CalendarDate.java
+++ jGTFS/src/ar/com/libreware/jgtfs/specification/component/CalendarDate.java
@@ -1,5 +1,91 @@
 package ar.com.libreware.jgtfs.specification.component;
 
-public class CalendarDate {
+import java.util.Date;
 
+import ar.com.libreware.jgtfs.specification.component.extra.ConstantEnum;
+
+public class CalendarDate extends AbstractComponent{
+	//TODO normalizar los enum para independizarlos (si comienzan en 1 o poeen otros valores identicos) y normalizarlos
+	
+	public static final class ExceptionType extends ConstantEnum{
+		public static final ExceptionType AddDate = new ExceptionType(1);
+		public static final ExceptionType DeleteDate= new ExceptionType(2);
+		
+		private ExceptionType(int value){
+			super(value);
+		}
+	}
+	
+	
+	private String idCalendar;
+	private Date date;
+	private ExceptionType exceptionType;
+
+	
+	public CalendarDate(String idCalendar, Date date, ExceptionType exceptionType) {
+		this.idCalendar = idCalendar;
+		this.date = date;
+		this.exceptionType = exceptionType;
+	}
+	
+
+	public CalendarDate(Calendar calendar, Date date, ExceptionType exceptionType) {
+		this(calendar.getId(),date,exceptionType);
+	}
+	
+ 
+
+	@Override
+	public boolean validate() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void copy(AbstractComponent otherComponent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int compareTo(AbstractComponent otherComponent) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean equals(AbstractComponent otherComponent) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+	public String getIdCalendar() {
+		return idCalendar;
+	}
+
+
+	public void setIdCalendar(String idCalendar) {
+		this.idCalendar = idCalendar;
+	}
+
+
+	public Date getDate() {
+		return date;
+	}
+
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+
+	public ExceptionType getExceptionType() {
+		return exceptionType;
+	}
+
+
+	public void setExceptionType(ExceptionType exceptionType) {
+		this.exceptionType = exceptionType;
+	}
 }
